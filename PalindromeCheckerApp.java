278e650200948eff476fea8bcd2d60693b4c80a8
mrunalshinde30 <ms4236@srmist.edu.in>
2026-02-23 10:18:10 +0530

    hardcoded string use case updated in new branch uc2 from main

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,21 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reverse = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reverse)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is Not a Palindrome");
+        }
+    }
+}
