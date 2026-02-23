7fedd1d6cd465a10507967aff59a9ff572d379c9
Soumya Suman <168551402+sugarsomee@users.noreply.github.com>
2026-02-23 11:05:59 +0530

    Refactor palindrome checker for 3rd use case

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,21 +1,21 @@
-public class UseCase2PalindromeCheckerApp {
+public class UseCase3PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Hardcoded string
-        String word = "madam";
+        // Original string
+        String original = "level";
 
-        // Reverse the string
-        String reverse = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reverse = reverse + word.charAt(i);
+        // Reverse the string using a loop
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        // Check palindrome using if-else
-        if (word.equals(reverse)) {
-            System.out.println(word + " is a Palindrome");
+        // Compare original and reversed strings
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome");
         } else {
-            System.out.println(word + " is Not a Palindrome");
+            System.out.println(original + " is Not a Palindrome");
         }
     }
 }
