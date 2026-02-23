ab4c40bbc0eb512acff28ca53241fdede222b649
sugarsomee <sugar.hts123@gmail.com>
2026-02-23 11:15:19 +0530

    string to char array uc4

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,21 +1,33 @@
-public class UseCase3PalindromeCheckerApp {
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         // Original string
-        String original = "level";
+        String word = "radar";
 
-        // Reverse the string using a loop
-        String reversed = "";
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        // Convert string to character array
+        char[] chars = word.toCharArray();
+
+        // Two-pointer approach
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // Compare original and reversed strings
-        if (original.equals(reversed)) {
-            System.out.println(original + " is a Palindrome");
+        // Display result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
         } else {
-            System.out.println(original + " is Not a Palindrome");
+            System.out.println(word + " is Not a Palindrome");
         }
     }
 }
